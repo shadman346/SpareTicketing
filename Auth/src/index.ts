f900f20f00d4a7fd2e05cebf0da4f78215ff5043
shadman346 <shadmanansari346c@gmail.com>
2021-12-13 15:34:58 +0530

    add jest supertest and few testing suits

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-import { Request,Response, NextFunction } from 'express';
-import {json} from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import { catchAsync } from './util/catchAsync';
-import cookieSession from 'cookie-session';
 import mongoose from 'mongoose';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-
-app.use(
-    cookieSession({
-        signed:false,
-        secure:true
-    })
-)
-app.use(currentUserRouter)
-app.use(signInRouter)
-app.use(signOutRouter)
-app.use(signUpRouter)
-
-app.get('*',catchAsync(async(req: Request,res: Response) =>{
-    throw new NotFoundError();
-}))
-
-app.use(errorHandler)
+import { app } from './app';
 
 const start = async() =>{
     if(!process.env.JWT_KEY){
