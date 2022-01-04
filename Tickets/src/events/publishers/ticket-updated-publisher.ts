9266e22401a1f5811f2620d5d1f8c6bb984e5992
shadman346 <shadmanansari346c@gmail.com>
2022-01-04 16:07:57 +0530

    tickets partially complete

---
+++ Tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@shad-tix/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
